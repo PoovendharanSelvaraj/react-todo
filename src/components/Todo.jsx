2aadf76f2f246747aa19bc4f9b2a9ee41610f15e
Poovendharan Selvaraj <97455003+PoovendharanSelvaraj@users.noreply.github.com>
2022-05-19 20:30:02 +0530

    TodoUpdated assignment 4

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import styles from "./Todo.module.css";
-import { TodoItem } from "./TodoItem";
 import { TodoList } from "./TodoList";
 
 export const Todo = () => {
   const [todo, setTodo] = useState("");
-
   const [todos, setTodos] = useState([]);
+  const [showAll, setShowAll] = useState(true);
+
   const handleClick = () => {
     todo !== ""
-      ? setTodos([...todos, { id: Date.now(), value: todo }])
+      ? setTodos([...todos, { id: Date.now(), status: false, value: todo }])
       : alert(`Enter string`);
     setTodo("");
   };
@@ -17,14 +17,24 @@ export const Todo = () => {
     let newTodo = todos.filter((todo) => todo.id !== id);
     setTodos(newTodo);
   };
-
+  const handleToggle = (id) => {
+    const updateTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, status: !todo.status } : todo
+    );
+    setTodos(updateTodos);
+  };
   return (
     <div className={styles.container}>
-      {todos.map((todo) => (
-        <TodoList>
-          <TodoItem key={todo.id} todo={todo} onDelete={onDelete} />
-        </TodoList>
-      ))}
+     {todos
+        .filter((todo) => (showAll ? true : !todo.status))
+        .map((todo) => (
+          <TodoList
+            key={todo.id}
+            handleToggle={handleToggle}
+            todo={todo}
+            onDelete={onDelete}
+          />
+        ))}
 
       <input
         value={todo}
@@ -35,6 +45,11 @@ export const Todo = () => {
         placeholder="Enter string"
       />
       <button onClick={handleClick}>+</button>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          {showAll ? "Show Unfinished only" : "Show all"}
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,3 +1,18 @@
-export const TodoList = ({ children }) => {
-  return <div>{children}</div>;
+import { useState } from "react";
+import styles from "./Todo.module.css";
+export const TodoList = ({ todo, onDelete, handleToggle }) => {
+  return (
+    <div className={styles.list} key={todo.id}>
+      <h2 className={todo.status ? styles.finish : null}>{todo.value}</h2>
+      <div className={styles.divin}>
+        <input
+          type="checkbox"
+          onChange={(e) => {
+            e.target.checked ? handleToggle(todo.id) : todo.status;
+          }}
+        />
+        <button onClick={() => onDelete(todo.id)}>-</button>
+      </div>
+    </div>
+  );
 };
